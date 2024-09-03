209a354fd94ff91fd1e7d28374a45307921890a8
Medha <smedha2002@gmail.com>
2024-09-03 23:23:56 +0530

    Complete UI

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+import toast from "react-hot-toast";
+
+import ProfileInfo from "../components/ProfileInfo";
+import Repos from "../components/Repos";
+import Search from "../components/Search";
+import SortRepos from "../components/SortRepos";
+import Spinner from "../components/Spinner";
 
 const HomePage = () => {
-  return (
-    <div>
-      Homepage
-    </div>
-  )
-}
+	const [userProfile, setUserProfile]= useState(null);
+	const [ repos, setRepos]=useState([]);
+	const [loading, setLoading]=useState(false);
+	const[sortType, setSortType]=useState("forks");
+	return (
+		<div className='m-4'>
+			<Search />
+			<SortRepos />
+			<div className='flex gap-4 flex-col lg:flex-row justify-center items-start'>
+				<ProfileInfo />
+				<Repos />
+				<Spinner />
+			</div>
+		</div>
+	);
+};
 
 export default HomePage
